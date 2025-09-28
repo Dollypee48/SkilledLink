541b633186d1753ed8497d3502d4e7d399c1a36b
Dollypee48 <akinolad22@gmail.com>
2025-09-28 12:10:33 +0100

    making changes in the cors

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:4173', 'https://skilled-link-af8m.vercel.app'],
+  origin: ['http://localhost:5173', 'http://localhost:4173', 'https://skilled-link-lund.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
